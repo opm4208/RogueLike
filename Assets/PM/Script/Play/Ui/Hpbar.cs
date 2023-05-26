9318289c4447918725e3016242f895c8ea69d47f
민 박 <opm4208@gmail.com>
2023-05-26 21:42:24 +0900

    feat: 완

--- Assets/PM/Script/Play/Ui/Hpbar.cs
+++ Assets/PM/Script/Play/Ui/Hpbar.cs
@@ -7,19 +7,19 @@ using UnityEngine.UI;
 public class Hpbar : MonoBehaviour
 {
     public GameObject Player;
-    private PlayerController controller;
+    private PlayerStatus controller;
     public Slider hp_slider;
 
     private void Awake()
     {
-        controller = Player.GetComponent<PlayerController>();
+        controller = Player.GetComponent<PlayerStatus>();
         hp_slider = GetComponent<Slider>();
         hp_slider.minValue = 0;
-        hp_slider.maxValue = controller.playermaxhp;
+        hp_slider.maxValue = controller.Hp;
     }
 
     private void Update()
     {
-        hp_slider.value = controller.playerhp;
+        hp_slider.value = controller.MaxHp;
     }
 }
